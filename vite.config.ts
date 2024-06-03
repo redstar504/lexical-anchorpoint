6d5031cd25bb2745bb9e1f822bf640592dc261d2
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-06-03 13:18:25 -0700

    used a dedicated non-standard port for demo server

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5172,
+    host: true,
+  },
   base: '/lexical-anchorpoint/',
   build: {
     outDir: 'dist'
