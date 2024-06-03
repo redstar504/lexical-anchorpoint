4637edf9a39a5ef38fc90a899b99f97f3a9e7c05
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-06-03 14:06:51 -0700

    updated demo page copy

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ import AnchorPointPlugin from './AnchorPoint/AnchorPointPlugin.tsx'
 import { createAnchorPoint } from './AnchorPoint/util.ts'
 import { DEFAULT_URL_REGEX } from './AnchorPoint/const.ts'
 import { IoIosArrowDroprightCircle } from 'react-icons/io'
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import bash from 'react-syntax-highlighter/dist/esm/languages/hljs/bash';
-import {Light as SyntaxHighlighter} from 'react-syntax-highlighter'
+import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript'
+import bash from 'react-syntax-highlighter/dist/esm/languages/hljs/bash'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { agate } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
-SyntaxHighlighter.registerLanguage('javascript', js);
-SyntaxHighlighter.registerLanguage('bash', bash);
+SyntaxHighlighter.registerLanguage('javascript', js)
+SyntaxHighlighter.registerLanguage('bash', bash)
 
 
 const ANCHOR_POINTS = [
@@ -43,21 +43,27 @@ function App() {
           <h1><FaAnchor /> Lexical AnchorPoint</h1>
           <p>This plugin aims to provide a drop-in replacement for the Lexical AutoLink plugin included with the Lexical
             React package. This plugin passes the most critical e2e tests of the original AutoLink plugin in only ~100
-            lines of <a href="https://github.com/redstar504/lexical-anchorpoint/blob/main/src/AnchorPoint/AnchorPointPlugin.tsx">easily understandable code</a>.</p>
+            lines of <a
+              href="https://github.com/redstar504/lexical-anchorpoint/blob/main/src/AnchorPoint/AnchorPointPlugin.tsx">easily
+              understandable code</a>.</p>
 
-          <p>Using rigorous <a href="https://github.com/redstar504/lexical-anchorpoint/blob/main/src/tests/expectations.ts">regex testing</a>, combined with a simple aggregate-matching node transformer, it easily
-            identifies links within TextNodes and converts them to AutoLink nodes. It solves the major shortcoming of
+          <p>Using <a href="https://github.com/redstar504/lexical-anchorpoint/blob/main/src/tests/expectations.ts">regex
+            testing</a>,
+            combined with a simple aggregate-matching node transformer, it provides a simple way to
+            identify and convert URLs within a span of TextNodes into AutoLink nodes. It solves the shortcoming of
             the AutoLink plugin’s inability to match some obvious URLs. The plugin is largely inspired by the original
             plugin, but at it’s core, chooses a different approach.</p>
 
           <p>This project is experimental and likely has a variety of issues. It meets the use case of a simple
             Lexical implementation being developed by the author. You are encouraged to test this plugin thoroughly and
-            report any issues in the GitHub repository. See additional contributing notes below.</p>
+            report any issues in the <a href="http://github.com/redstar504/lexical-anchorpoint">GitHub repository</a>.
+            Emails
+            have not been tested yet, but you are welcome to clone the repo and try on your own, using the original plugin's regex.</p>
         </section>
 
         <section>
           <h2>Try it and see</h2>
-          <p>Note: The floating link plugin is not installed, nor is any click handling, so this demo does not
+          <p>Note: The floating link plugin is not installed, nor is any click handling, so this demo will not
             let you click on the links yet. 😆</p>
           <LexicalComposer initialConfig={initialConfig}>
             <div id="editorContainer">
@@ -87,20 +93,20 @@ function App() {
         <section>
           <h2>Typical Installation</h2>
           <p>Run the following command in your Lexical React project’s root:</p>
-          <SyntaxHighlighter language="bash" style={agate} customStyle={{padding: '1em'}}>
+          <SyntaxHighlighter language="bash" style={agate} customStyle={{ padding: '1em' }}>
             {`npm i lexical-anchorpoint`}
           </SyntaxHighlighter>
           <p>Create the matchers, which in this plugin are referred to as anchor points, that specify the URL
             patterns:</p>
-          <SyntaxHighlighter language="react" style={agate} customStyle={{padding: '1em'}}>
-{`const ANCHOR_POINTS = [
+          <SyntaxHighlighter language="react" style={agate} customStyle={{ padding: '1em' }}>
+            {`const ANCHOR_POINTS = [
   createAnchorPoint(DEFAULT_URL_REGEX, text => {
   return text.startsWith('http') ? text : \`https://\${text}\`})
 ]`}
           </SyntaxHighlighter>
 
           <p>Nest the plugin as a child of your Lexical Composer component:</p>
-          <SyntaxHighlighter language="react" style={agate} customStyle={{padding: '1em'}}>
+          <SyntaxHighlighter language="react" style={agate} customStyle={{ padding: '1em' }}>
             {`<AnchorPointPlugin points={ANCHOR_POINTS} />`}
           </SyntaxHighlighter>
         </section>
@@ -112,8 +118,8 @@ function App() {
             the most commonly reported issues.</p>
 
           <p>There were some tests that were deemed unimportant for the purposes of an early release of this
-            implementation, and therefore were not included in this project’s e2e tests:</p>
-
+            implementation, and therefore were not included in this project’s e2e tests (in fact none have been
+            implemented in this repo yet, it was developed against the playground tests and copied over):</p>
           <ul>
             <li>
               <IoIosArrowDroprightCircle />
@@ -136,19 +142,22 @@ function App() {
 
         <section>
           <h2>Additional Information</h2>
-          <p>Lexical's React package ships with a plugin called AutoLink. The goal of AutoLink is to provide a node
-            transformer that transforms any link like text typed or pasted into the editor into an AutoLink node.</p>
+          <p>As you probably know, Lexical's React package ships with a plugin called AutoLink. The goal of AutoLink is
+            to provide a node
+            transformer that transforms any URL like text typed or pasted into the editor into an AutoLink node.</p>
 
-          <p>Several issues have been reported due to the AutoLink plugin’s inability to match some simple URLs. A common
-            example is when typing a URL with a subdomain, such as http://www.google.com in the editor, it does not
-            include the `.com` portion as being part of the link.</p>
+          <p>Several issues have been reported due to the AutoLink plugin’s inability to match some simple URLs. A
+            common example is when typing a URL with a subdomain, such as http://www.google.com in the editor.
+            Unfortunately, it does not
+            include the `.com` portion as being part of the link (the biggest source of motivation for this
+            project).</p>
 
-          <p>The author of this plugin originally set out to contribute a resolution to the issues identified with the
-            AutoLink plugin. After reviewing the variety of PRs that have been raised to solve the core issues, and
-            their corresponding nuances, he determined that it would be easier to rewrite a simple implementation, since
-            the same functionality could be achieved with a lot less code, while meeting the use case of his project.</p>
+          <p>The author of this plugin set out to contribute a resolution to the issues identified with the
+            AutoLink plugin. After reviewing the variety of PRs that have been raised to solve the popular issues, and
+            their corresponding nuances, he determined that it would be easier to rewrite a simple implementation with a
+            focus on more powerful regex, combined with a lot less code.</p>
 
-          <p>Perhaps in the future, if this plugin is able to exceed the standards of the original AutoLink plugin, in a
+          <p>Perhaps in the future, if this plugin is able to exceed the standards of the original AutoLink plugin in a
             reliably tested fashion, it can be integrated into the core Lexical package.</p>
         </section>
 
@@ -158,7 +167,11 @@ function App() {
             author.</p>
           <p>As such, there are likely many edge cases, and bugs that have not been identified. You are encouraged to
             raise
-            an issue or a PR, to report any problems, or provide any missing functionality.</p>
+            an issue or a PR, to report any problems, or to provide any missing functionality.</p>
+
+          {/*<p>Any fixes or feature additions must be backed by tests, no exceptions.  There are currently no e2e tests
+          included in this repo since it was developed directly against the playground tests, but the critical ones are in the process
+          of being ported over.</p>*/}
 
           <a href="https://github.com/redstar504/lexical-anchorpoint" target="_blank" id="contribLink">
             <FaGithub />
