1c8ca31a18154462710f932216febdca6ef89baf
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-06-04 13:16:27 -0700

    added a test for appending text content

--- tests/e2e/anchor.spec.ts
+++ tests/e2e/anchor.spec.ts
@@ -51,5 +51,28 @@ test('typing links into the editor', async ({page}) => {
 })
 
 test('appending to URL when text content is not URL content', async ({page}) => {
+  await page.goto(`http://localhost:${E2E_PORT}/lexical-anchorpoint/`)
+  const editor = page.locator('div[contenteditable=true]').first()
+  await editor.focus()
 
+  await page.keyboard.type('http://example.com')
+  await page.keyboard.press('ArrowLeft');
+  await page.keyboard.type(' ')
+  await page.keyboard.press('ArrowRight');
+  await page.keyboard.type('. More text.')
+
+  const html = await page.locator('div[contenteditable=true]').first().innerHTML()
+
+  const expectedHtml = `
+  <p dir="ltr">
+    <a href="http://example.com" dir="ltr">
+      <span data-lexical-text="true">http://example.co m</span>
+    </a>
+    <span data-lexical-text="true">. More text.</span>
+  </p>
+  `
+
+  const expected = await prettifyHTML(expectedHtml.replace(/\n/gm, ''))
+  const actual = await prettifyHTML(html.replace(/\n/gm, ''))
+  expect(actual).toEqual(expected)
 })
