b3baf4dcc0df5ad0a0e9cfd6f5587de593eaaa52
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-06-04 14:06:11 -0700

    added test for pasting URLs separated by separators

--- tests/e2e/anchor.spec.ts
+++ tests/e2e/anchor.spec.ts
@@ -77,7 +77,7 @@ test('appending to URL when text content is not URL content', async ({page}) => 
   expect(actual).toEqual(expected)
 })
 
-test('creates links when pasting text with URLs', async({page}) => {
+test('pasting multiple urls separated by text or spaces', async({page}) => {
   await page.goto(`http://localhost:${E2E_PORT}/lexical-anchorpoint/`)
   const editor = page.locator('div[contenteditable=true]').first()
   await editor.focus()
@@ -93,7 +93,7 @@ test('creates links when pasting text with URLs', async({page}) => {
     Object.defineProperty(pasteEvent, 'clipboardData', {
       value: {
         files: [],
-        getData(type, value) {
+        getData() {
           return 'Hello http://example.com and https://example.com/path?with=query#and-hash and www.example.com'
         },
         types: ['text/plain']
@@ -120,6 +120,55 @@ test('creates links when pasting text with URLs', async({page}) => {
     <span data-lexical-text="true">and</span>
     <a href="https://www.example.com" dir="ltr">
       <span data-lexical-text="true">www.example.com</span>
+    </a>
+  </p>
+  `
+
+  const expected = await prettifyHTML(expectedHtml.replace(/\n/gm, ''))
+  const actual = await prettifyHTML(html.replace(/\n/gm, ''))
+  expect(actual).toEqual(expected)
+})
+
+test('pasting links separated by characters such as commas', async({page}) => {
+  await page.goto(`http://localhost:${E2E_PORT}/lexical-anchorpoint/`)
+  const editor = page.locator('div[contenteditable=true]').first()
+  await editor.focus()
+
+  await page.evaluate(async() => {
+    const editorElem = document.querySelector('div[contenteditable=true]')
+
+    const pasteEvent = new Event('paste', {
+      bubbles: true,
+      cancelable: true
+    })
+
+    Object.defineProperty(pasteEvent, 'clipboardData', {
+      value: {
+        files: [],
+        getData() {
+          return 'https://1.com/,https://2.com/;;;https://3.com'
+        },
+        types: ['text/plain']
+      }
+    })
+
+    editorElem.dispatchEvent(pasteEvent)
+  })
+
+  const html = await page.locator('div[contenteditable=true]').first().innerHTML()
+
+  const expectedHtml = `
+  <p>
+    <a href="https://1.com/" dir="ltr">
+      <span data-lexical-text="true">https://1.com/</span>
+    </a>
+    <span data-lexical-text="true">,</span>
+    <a href="https://2.com/" dir="ltr">
+      <span data-lexical-text="true">https://2.com/</span>
+    </a>
+    <span data-lexical-text="true">;;;</span>
+    <a href="https://3.com" dir="ltr">
+      <span data-lexical-text="true">https://3.com</span>
     </a>
   </p>
   `
